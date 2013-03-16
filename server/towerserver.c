07abb2d24bccb8b247538a6abb8ab2107c9406dd
Josh Armstrong <theartistformerlyknownasjosh@gmail.com>
2013-03-15 21:56:34 -0700

    Animation lists work

--- server/towerserver.c
+++ server/towerserver.c
@@ -4,12 +4,15 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <dirent.h>
 #include <jansson.h>
 #include "mongoose.h"
 
-char *
-
 static void send_message(struct mg_connection *conn, char *msg, size_t msg_len);
+static void tl_send_error(const char *errormsg, struct mg_connection *conn);
+static void tl_send_animationlist(struct mg_connection *conn);
+
+static char rootdir[512] = "/home/josh/Documents/Tower Lights Animations/";
 
 /**
  * Sends the list animatinos to the client.
@@ -19,17 +22,74 @@ static void tl_send_animationlist(struct mg_connection *conn) {
   json_t *array;
   char *msg;
 
-  //root = json_pack("{s:s,s:[{s:s,s:s}]}", "type", "animationlist", "animations", "title", "Test Animation", "musicfile", "animations/testanimation/music.wav"), 
   root = json_object();
-  json_object_set(root, "type", json_string("animationlist"));
+  json_object_set_new(root, "type", json_string("animationlist"));
   array = json_array(); 
 
-  // Goal: Each folder in the specified animations folder holds an animation and it's music. Enumerate all the animations their music files. 
-   
+  // Goal: Each folder in the specified animations folder holds an animation and it's music. 
+  // Enumerate all the animations their music files
+  DIR *dirp = opendir(rootdir);
+  struct dirent *dp;
+
+  // Check if the directory exists
+  if (dirp == NULL) {
+    printf("Could not animation directory '%s'.\n", rootdir);
+    tl_send_error("Could not open root animation directory", conn);
+    return;
+  }
+
+  // Iterate over the directories
+  while ((dp = readdir(dirp)) != NULL) {
+    if (dp->d_type == DT_DIR) {
+      if (strncmp(dp->d_name, ".", 1) != 0 &&
+          strncmp(dp->d_name, "..", 2) != 0) {
+        json_t *animation = json_object();       
+        json_object_set_new(animation, "title", json_string(dp->d_name));
+  
+        printf("Animation title: %s\n", dp->d_name); 
+
+        char newdir[100]; 
+        strncpy(newdir, rootdir, 100);
+        strcat(strcat(newdir, "/"), dp->d_name);
+
+        DIR *dirp2 = opendir(newdir);
+        struct dirent *dp2;
+        int foundtan = 0;
+        int foundwav = 0;
+
+        // Look for one wav file and one tan file
+        while ((dp2 = readdir(dirp2)) != NULL) {
+          if (dp2->d_type == DT_REG) {
+            if (strncmp((dp2->d_name + strlen(dp2->d_name) - 4), ".wav", 4) == 0) {
+              if (foundwav == 1) {
+                printf("  Error: multiple wav files in a directory\n");
+              } else {
+                // printf("  wav file: %s\n", dp2->d_name);
+                json_object_set_new(animation, "music", json_string(dp2->d_name));
+              }
+              foundwav = 1;
+            } else if (strncmp((dp2->d_name + strlen(dp2->d_name) - 4), ".tan", 4) == 0) {
+              if (foundtan == 1) {
+                printf("  Error: multiple tan files in a directory\n");
+              } else {
+                // printf("  tan file: %s\n", dp2->d_name);
+                json_object_set_new(animation, "tan", json_string(dp2->d_name));
+              }
+              foundtan = 1;
+            }
+          }
+        }
+
+        json_array_append_new(array, animation);
+        closedir(dirp2);
+      }
+
+    }
+  }
+
+  closedir(dirp);
 
   json_object_set(root, "animations", array);
-
-  
 
   msg = json_dumps(root, 0);
   if (msg == NULL) {
@@ -71,25 +131,44 @@ static void tl_send_error(const char *errormsg, struct mg_connection *conn) {
   json_decref(root); 
 }
 
+
 /** 
  * Sends a message to the client.
  */
 static void send_message(struct mg_connection *conn, char *msg, size_t msg_len) {
-  unsigned char reply[200];
+  unsigned char *reply;
   int i;
+  int offset = 0;
 
-  if (msg_len > 125) {
-    msg_len = 125;
-  }
-
+  reply = (unsigned char *)malloc(sizeof(unsigned char) * msg_len);
   reply[0] = 0x81;  // text, FIN set
-  reply[1] = msg_len;
+
+  if (msg_len <= 125) {
+    reply[1] = msg_len;
+    offset = 2;
+  } else if (msg_len <= 65535) {
+    reply[1] = 126;
+    reply[2] = (msg_len & 0xff00) >> 8;
+    reply[3] = msg_len & 0x00ff;
+    offset = 4;
+  } else if (msg_len <= 0xffffffffffffffff) {
+    reply[1] = 127;
+    unsigned long long mask =  0xff00000000000000;
+    for (i = 0; i < 8; i++) {
+      reply[i+2] = (msg_len & mask) >> ((7-i) * 8);
+      mask = mask >> 16;
+    }
+    offset = 10;
+  } else {
+    printf("Error: message is too large to send.\n");
+    tl_send_error("Error sending message: payload too large", conn);
+  }
 
   for (i = 0; i < msg_len; i++) {
-    reply[i+2] = msg[i];
+    reply[i+offset] = msg[i];
   }
 
-  mg_write(conn, reply, 2 + msg_len);
+  mg_write(conn, reply, offset + msg_len);
 }
 
 /**
@@ -185,3 +264,4 @@ int main(void) {
 
   return 0;
 }
+
